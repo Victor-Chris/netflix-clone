4014a6c3024987c8c3aadf30d0880d55fc1d0286
Victor-Chris <victorchris51@gmail.com>
2021-02-22 16:06:08 +0300

    Edited Row component to play show trailers from YouTube

--- src/components/row/Row.tsx
+++ src/components/row/Row.tsx
@@ -1,5 +1,7 @@
 import react, { useEffect, useState } from 'react';
 import instance from '../../axios';
+import YouTube from 'react-youtube';
+import movieTrailer from 'movie-trailer';
 import './Row.css';
 
 interface rowProps {
@@ -11,7 +13,8 @@ interface rowProps {
 const poster_base_url = 'https://image.tmdb.org/t/p/original/';
 
 const Row = (props: rowProps) => {
-    const [movies, setMovies] = useState<[]>([]);;
+    const [movies, setMovies] = useState<[]>([]);
+    const [trailerUrl, setTrailerUrl] = useState<string | null>('');
 
     // Runs on a specific condition
     useEffect(() => {
@@ -25,6 +28,29 @@ const Row = (props: rowProps) => {
         fetchData();
     }, [props.fetchURL]);
 
+    const opts = {
+        height: "390",
+        width: "100%",
+        playerVars: {
+            autoplay: 1
+        }
+    };
+
+    const handleClick = (movie: any) => {
+        if (trailerUrl) {
+            setTrailerUrl('');
+        } else {
+            movieTrailer(movie?.name || "")
+            .then((url: string) => {
+                const urlParams = new URLSearchParams(new URL(url).search);
+                setTrailerUrl(urlParams.get('v'));
+            })
+            .catch((error: any) => {
+                console.log(error);
+            });
+        }
+    };
+
     return (
         <div className="row">
             {/** Row Title */}
@@ -35,11 +61,14 @@ const Row = (props: rowProps) => {
                 {movies.map((movie: any) => (
                     <img 
                         key={movie.id}
+                        onClick={() => handleClick(movie)}
                         className={`row__poster ${props.isLargeRow && "row__posterLarge"}`}    
                         src={`${poster_base_url}${props.isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name} 
                     />
                 ))}
             </div>
+
+            {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
 
         </div>
     )
